b2fccb3571cef56d8ecc37ddba0ec0b870da331c
tzhmbtv <elnur9924@gmail.com>
2019-01-21 15:25:39 +0600

    Removing global variables from TicketController

--- app/Http/Controllers/TicketController.php
+++ app/Http/Controllers/TicketController.php
@@ -9,34 +9,35 @@ use Auth;
 
 class TicketController extends Controller
 {
-    protected $ticket;
     protected $mailFrom;
     protected $mailTo;
     protected $category;
 
     public function index(Request $request, $hash)
     {
-        $this->ticket = Ticket::where('hash', $hash)->first();
-        if ($this->ticket->ticket_status == 1) {
-            $this->ticket->ticket_status = 2;
-            $this->ticket->save();
+        $ticket = Ticket::where('hash', $hash)->first();
+        if ($ticket->ticket_status == 1) {
+            $ticket->ticket_status = 2;
+            $ticket->save();
         }
-        return view('ticket', ['ticket' => $this->ticket]);
+        return view('ticket', ['ticket' => $ticket]);
     }
     public function process(Request $request, $hash)
     {
-        $this->ticket = Ticket::where('hash', $hash)->first();
-        $this->ticket->ticket_status = 3;
-        $this->ticket->save();
+        $ticket = Ticket::where('hash', $hash)->first();
+        $ticket->ticket_status = 3;
+        $ticket->save();
+        $this->Answered($hash);
         return back();
     }
-    public function Answered()
+    public function Answered($hash)
     {
-        $this->category = $this->ticket->category->name;
-        $hashLink = "http://127.0.0.1:8000/ticket/{$this->ticket->hash}";
+        $ticket= Ticket::where('hash', $hash)->first();
+        $this->category = $ticket->category->name;
+        $hashLink = "http://127.0.0.1:8000/ticket/$hash";
         $message_Raw = "Мы ответили на ваш вопрос, можете посмотреть ответ на него здесь - $hashLink";
-        $this->mailTo = $this->ticket->email;
-        $this->mailFrom = Auth::user()->email;
+        $this->mailTo = $ticket->email;
+        $this->mailFrom = "lifeinternchoco@gmail.com";
         Mail::raw($message_Raw, function ($message) {
             $message->from($this->mailFrom);
             $message->to($this->mailTo)->subject("Ответ на ваш $this->category");
